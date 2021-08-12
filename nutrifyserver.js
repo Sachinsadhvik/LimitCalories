587e3d7d091772f07badd298aa3a760bd83b8c36
Sachinsadhvik <sachindagrt88@gmail.com>
2021-08-12 14:03:19 +0530

    changed ui

--- nutrifyserver.js
+++ nutrifyserver.js
@@ -2,11 +2,12 @@ const express =require("express");
 var app= new express();
 
 var index= require("./ROUTES/index")
+port = process.env.PORT || 3999
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.set('views','VIEW');
 app.set('view engine',"ejs");
-port = process.env.PORT || 3999
+
 
 app.use(express.static('STATIC'))
 app.use('/', index);
